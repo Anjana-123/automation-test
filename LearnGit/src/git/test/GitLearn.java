7b4dcec8896046e7913ba77348b0038f0260338e
User <User@Akku>
2020-05-15 21:20:23 +0530

    new

--- LearnGit/src/git/test/GitLearn.java
+++ LearnGit/src/git/test/GitLearn.java
@@ -5,7 +5,7 @@ public class GitLearn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("new branch anjana");
+System.out.println("tests");
 System.out.println("added");
 System.out.println("integrated with jenkins");
 System.out.println("testing one more commit");
